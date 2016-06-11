e3c4b728877a5ced947b64637be799d8ca736ee7
JingRong ( <jing_rong92@hotmail.com>
2016-06-11 15:15:25 -0700

    API end points for available/pending/completed tasks

--- routes/api/v1/bruddrtask.js
+++ routes/api/v1/bruddrtask.js
@@ -8,6 +8,31 @@ const middleware = require('./../../../middleware')
 router.get('/getAvailableTasks', middleware.isAuthenticated, function(req,res,next) {
   // Get the params for the query
   var searchQuery = req.query
+  searchQuery['status'] = 0
+
+  bruddrTask.find(searchQuery , function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+})
+
+router.get('/getPendingTasks', middleware.isAuthenticated, function(req,res,next) {
+  // Get the params for the query
+  var searchQuery = req.query
+  searchQuery['status'] = 1
+  searchQuery['bruddr_name'] = req.session.currentUser
+
+  bruddrTask.find(searchQuery , function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+})
+
+router.get('/getCompletedTasks', middleware.isAuthenticated, function(req,res,next) {
+  // Get the params for the query
+  var searchQuery = req.query
+  searchQuery['status'] = 2
+  searchQuery['bruddr_name'] = req.session.currentUser
 
   bruddrTask.find(searchQuery , function (err, post) {
     if (err) return next(err);
