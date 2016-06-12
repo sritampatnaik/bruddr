09ae523f515ca33d997b771cb27ca53d85c26553
JingRong ( <jing_rong92@hotmail.com>
2016-06-11 17:44:18 -0700

    Refactor to use 1 api call only

--- routes/api/v1/bruddrtask.js
+++ routes/api/v1/bruddrtask.js
@@ -5,34 +5,13 @@ var bruddrTask = require('./../../../models/bruddrTask');
 
 const middleware = require('./../../../middleware')
 
-router.get('/getAvailableTasks', middleware.isAuthenticated, function(req,res,next) {
+router.get('/getTasks', middleware.isAuthenticated, function(req,res,next) {
   // Get the params for the query
   var searchQuery = req.query
-  searchQuery['status'] = 0
-
-  bruddrTask.find(searchQuery , function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-})
-
-router.get('/getPendingTasks', middleware.isAuthenticated, function(req,res,next) {
-  // Get the params for the query
-  var searchQuery = req.query
-  searchQuery['status'] = 1
-  searchQuery['bruddr_name'] = req.session.currentUser
-
-  bruddrTask.find(searchQuery , function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-})
-
-router.get('/getCompletedTasks', middleware.isAuthenticated, function(req,res,next) {
-  // Get the params for the query
-  var searchQuery = req.query
-  searchQuery['status'] = 2
-  searchQuery['bruddr_name'] = req.session.currentUser
+  // Check if it's a non-generic query (not just available tasks)
+  if (req.query.status > 0) {
+    searchQuery['bruddr_name'] = req.session.currentUser
+  }
 
   bruddrTask.find(searchQuery , function (err, post) {
     if (err) return next(err);
